321705d8a4d346fb3a0e71d6b4da33d7ac1476ff
Dami <damian.garciagarcia@gmail.com>
2015-06-03 19:18:17 +0200

    Scaffolding for bookshelf project

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bookshelfApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
